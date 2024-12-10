47173ff9733e39dfdba1fe2c488d552f96c62a0d
Cauã Campos <campos.eet@gmail.com>
2024-12-10 18:38:48 -0300

    Otimization

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,28 +2,32 @@ export default {
     COLORS: {
       WHITE: '#FFFFFF',
   
-      GREEN_700: '#00875F',
-      GREEN_500: '#00B37E',
+      GREEN_LIGHT: '#E5F0DB',
+      GREEN_MID: '#CBE4B4',
+      GREEN_DARK: '#639339',
   
-      RED: '#F75A68',
-      RED_DARK: '#AA2834',
+      RED_MID: '#F3BABD',
+      RED_LIGHT: '#F4E6E7',
+      RED_DARK: '#BF3B44',
   
-      GRAY_700: '#121214',
-      GRAY_600: '#202024',
-      GRAY_500: '#29292E',
-      GRAY_400: '#323238',
-      GRAY_300: '#7C7C8A',
-      GRAY_200: '#C4C4CC',
-      GRAY_100: '#E1E1E6'
+      GRAY_700: '#FAFAFA',
+      GRAY_600: '#EFF0F0',
+      GRAY_500: '#DDDEDF',
+      GRAY_400: '#B9BBBC',
+      GRAY_300: '#5C6265',
+      GRAY_200: '#333638',
+      GRAY_100: '#1B1D1E'
     },
     FONT_FAMILY: {
       REGULAR: 'Roboto_400Regular',
       BOLD: 'Roboto_700Bold'
     },
     FONT_SIZE: {
+      SSM: 12,
       SM: 14,
       MD: 16,
       LG: 18,
-      XL: 24
+      XL: 24,
+      XXL: 32
     }
   };
