c89c3784ce959343aecbdfe4a957ce68e1a777cd
Cauã Campos <campos.eet@gmail.com>
2024-12-10 22:46:57 -0300

    Atts

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,8 +1,12 @@
 import { Container, LoadIndicator } from './styles'
+import { StatusBar } from 'react-native';
 
 export function Loading() {
     return (
         <Container>
+            <StatusBar
+                barStyle="light-content"
+            />
             <LoadIndicator/>
         </Container>
     )
